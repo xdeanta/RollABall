17287cdd40d354a09e48e70d57d246d979d5c55c
Xavier <xdeantabelisario@danielcastelao.org>
2021-01-23 16:56:30 +0100

    Condicion de perdida de puntos y de la partida añadida

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -4,15 +4,22 @@ public class Enemy : MonoBehaviour
 {
     private NavMeshAgent pathfinder;
     private Transform target;
+    private Vector3 OriginalPos;
     
     void Start()
     {
         pathfinder = GetComponent<NavMeshAgent>();
+        OriginalPos = transform.position;
         target = GameObject.Find("Player").transform;
     }
     void Update()
     {
         pathfinder.SetDestination(target.position);
         Debug.Log(target.position);
+    }
+    private void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("Player")){
+            transform.position = OriginalPos;
+        }
     }
 }
--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -6,33 +6,49 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
+    public float speed = 10;
     // Start is called before the first frame update
     private Rigidbody rb;
     private int count;
+    private int points;
     private float movementX;
     private float movementY;
+    private Vector3 OriginalPos;
     public TextMeshProUGUI CountText;
     public GameObject WinTextObject;
+    public GameObject LoseTextObject;
 
     void Start()
     {
         rb = GetComponent <Rigidbody>();
         count = 0;
-        setCountText();
+        points = 0;
         WinTextObject.SetActive(false);
+        LoseTextObject.SetActive(false);
+        setCountText();
+        OriginalPos = transform.position;
+        /*if (SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            Input.gyro.enabled = true;
+        }*/
     }
 
     void OnMove(InputValue movementValue){
         Vector2 movementVector = movementValue.Get < Vector2 >();
         movementX = movementVector.x;
         movementY = movementVector.y;
+        //movementX = Input.acceleration.x;
+        //movementY = -Input.acceleration.y;
     }
 
     void setCountText(){
-        CountText.text = "Count: " + count.ToString();
-        if(count >= 8){
+        CountText.text = "Points: " + points.ToString();
+        if(count >= 8 && points == 8){
             WinTextObject.SetActive(true);
+        }else{
+            if(count >= 8 && points < 8){
+                LoseTextObject.SetActive(true);
+            }
         }
     }
 
@@ -45,6 +61,14 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.CompareTag("PickUp")){
             other.gameObject.SetActive(false);
             count++;
+            points++;
+            setCountText();
+        }
+        if(other.gameObject.CompareTag("Enemy")){
+            transform.position = OriginalPos;
+            if(points > 0){
+                points--;
+            }
             setCountText();
         }
     }
